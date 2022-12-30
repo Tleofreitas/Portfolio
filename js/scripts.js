ee110bc459bd55d8724e4e7c13f1a6675df80a03
Thiago Freitas <thiagoleonardo1995@live.com>
2022-12-30 16:10:55 -0300

    Dados das Competências

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// Hamburguer
+function clickMenu() {
+  if (itens.style.display == 'block') {
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'block'
+  }
+}
